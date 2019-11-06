c08fa534c8574804922a07c7fd4fbb168e9c832e
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-11-06 16:32:52 -0700

    Update clean-up-os.sh

--- clean-up-os.sh
+++ clean-up-os.sh
@@ -3,15 +3,16 @@
 set -e
 
 rm -f /home/ubuntu/.bash_history
-rm -f /home/ubuntu/.bash_history
+rm -f /home/ubuntu/.mysql_history
 rm -f /home/ubuntu/.viminfo
 rm -rf /home/ubuntu/.vim
 
 rm -f ~/.bash_history
 rm -f ~/.mysql_history
-#rm -f /etc/machine-id
-rm -f /var/log/**/*.{1..9}
-rm -f /var/log/**/*.gz
+rm -f ~/.viminfo
+rm -rf ~/.vim
+find /var/log -type f -name "*.gz" -delete
+find /var/log -type f -name "*.[1-9]" -delete
 
 apt-get clean
 apt-get autoclean
